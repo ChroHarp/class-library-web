95822294539cc5691d3fe5665940b30ebdaa8c9b
ChroHarp Chen <ChroHarp@gmail.com>
2025-06-20 16:12:50 +0800

    vite build-2

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,14 @@
-// src/firebaseConfig.js  —— 只做一件事：初始化 Firebase
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage }   from 'firebase/storage';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey       : import.meta.env.FIREBASE_API_KEY,
-  projectId    : import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  authDomain   : `${import.meta.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  apiKey:             import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:         import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:          import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:      import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
 };
 
-const app     = initializeApp(firebaseConfig);
-export const db      = getFirestore(app);
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const storage = getStorage(app);
